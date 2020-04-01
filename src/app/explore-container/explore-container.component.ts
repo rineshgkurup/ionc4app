563909425113e0b78d6e34d67e8da24ed79095c7
rinesh <rineshgopal@gmail.com>
2020-04-01 18:16:53 +0530

    editing tab

--- src/app/explore-container/explore-container.component.ts
+++ src/app/explore-container/explore-container.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { DatePicker } from '@ionic-native/date-picker/ngx';
 
 @Component({
   selector: 'app-explore-container',
@@ -8,8 +9,19 @@ import { Component, OnInit, Input } from '@angular/core';
 export class ExploreContainerComponent implements OnInit {
   @Input() name: string;
 
-  constructor() { }
+  constructor(private datePicker: DatePicker) { }
 
   ngOnInit() {}
+  
+  handleClick(event){
+	this.datePicker.show({
+	  date: new Date(),
+	  mode: 'date',
+	  androidTheme: this.datePicker.ANDROID_THEMES.THEME_HOLO_DARK
+	}).then(
+	  date => console.log('Got date: ', date),
+	  err => console.log('Error occurred while getting date: ', err)
+	);
+  }
 
 }
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { DatePicker } from '@ionic-native/date-picker/ngx';
 
 @Component({
   selector: 'app-tab1',
@@ -8,16 +7,6 @@ import { DatePicker } from '@ionic-native/date-picker/ngx';
 })
 export class Tab1Page {
 
-  constructor(private datePicker: DatePicker) {}
+  constructor() {}
 
-  handleClick(event){
-	this.datePicker.show({
-	  date: new Date(),
-	  mode: 'date',
-	  androidTheme: this.datePicker.ANDROID_THEMES.THEME_HOLO_DARK
-	}).then(
-	  date => console.log('Got date: ', date),
-	  err => console.log('Error occurred while getting date: ', err)
-	);
-  }
 }
